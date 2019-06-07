f28c0c6141b725b95332252144d843a0f581d54b
Abdul <awsayed@alaska.edu>
2019-06-07 04:29:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,5 +5,13 @@ class CashRegister
     @discount = discount
     @total = 0
   end
+  
+  def add_item(title, price, quantity=0)
+  if quantity > 1
+    @total += (price * quantity)
+  else
+    @total += price
+  end
+end
 
 end

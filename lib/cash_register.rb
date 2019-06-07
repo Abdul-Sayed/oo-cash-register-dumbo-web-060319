0a21a8879bb9d906473d59be4fdb37247849fb01
Abdul <awsayed@alaska.edu>
2019-06-07 04:19:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,8 @@
 class CashRegister
+  attr_accessor :discount
 
-def initialize(discount=0)
-  @discount = discount
-  @total = 0
-end
-
-
-
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
 end
